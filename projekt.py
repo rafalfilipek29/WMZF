d37e0ae616a838295c77e21856b7ffa5bba79bad
rafalfilipek29 <74666174+rafalfilipek29@users.noreply.github.com>
2020-12-11 12:56:34 +0100

    Add files via upload
    dodaliśmy możliwość podania prędkości i przyśpieszenia w trzech wymiarach

---
+++ projekt.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+pr_swiatla = 299792458
+c = 1
+
+
+class Czastka():
+    predkosc_ul = None
+    predkosc_ms = None
+    masa_spoczynkowa = None
+    masa_relatywistyczna = None
+    przyspieszenie_ul = None
+    przyspieszenie_ms = None
+    wektor_predkosci = None
+
+    def __init__(self, pr, mas_spo, przysp=0, typ=0, trojwymiar=0, pr_x=0, pr_y=0, pr_z=0, a_x=0, a_y=0, a_z=0):
+        if trojwymiar==0:
+            if typ == 0:
+                while pr >= 1:
+                    pr = float(input('Podaj poprawna predkosc (mniej niz 1)'))
+                self.predkosc_ul = pr
+                self.predkosc_ms = pr * pr_swiatla
+                self.przyspieszenie_ul = przysp
+                self.przyspieszenie_ms = przysp * pr_swiatla
+            else:
+                while pr >= pr_swiatla:
+                    pr = float(input('Podaj poprawna predkosc (mniej niz 299 792 458 m/s)'))
+                self.predkosc_ul = pr / pr_swiatla
+                self.predkosc_ms = pr
+                self.przyspieszenie_ms = przysp
+                self.przyspieszenie_ul = przysp / pr_swiatla
+        else:
+            if typ==0:
+                while abs(pr_x) >= 1:
+                    pr_x = float(input('Podaj poprawna predkosc (mniej niz 1)'))
+                while abs(pr_y) >= 1 or ((pr_x**2)+(pr_y**2))**(0.5)>=1:
+                    pr_y = float(input('Podaj poprawna predkosc (mniej niz 1)'))
+                while abs(pr_z) >= 1 or ((pr_x**2)+(pr_y**2)+(pr_z**2))**(0.5)>=1:
+                    pr_z = float(input('Podaj poprawna predkosc (mniej niz 1)'))
+                self.predkosc_ul=((pr_x**2)+(pr_y**2)+(pr_z**2))**(0.5)
+                self.predkosc_ms = ((pr_x**2)+(pr_y**2)+(pr_z**2)) * pr_swiatla
+                self.przyspieszenie_ul = ((a_x**2)+(a_y**2)+(a_z**2))**(0.5)
+                self.przyspieszenie_ms = ((a_x**2)+(a_y**2)+(a_z**2))**(0.5) * pr_swiatla
+                wektor=np.array([pr_x,pr_y,pr_z])
+                self.wektor_predkosci=wektor/wektor.max()
+            else:
+                while abs(pr_x) >= pr_swiatla:
+                    pr_x = float(input('Podaj poprawna predkosc (mniej niz 299 792 458 m/s)'))
+                while abs(pr_y) >= pr_swiatla or ((pr_x**2)+(pr_y**2))**(0.5)>=pr_swiatla:
+                    pr_y = float(input('Podaj poprawna predkosc (mniej niz 299 792 458 m/s)'))
+                while abs(pr_z) >= pr_swiatla or ((pr_x**2)+(pr_y**2)+(pr_z**2))**(0.5)>=pr_swiatla:
+                    pr_z = float(input('Podaj poprawna predkosc (mniej niz 299 792 458 m/s)'))
+                self.predkosc_ms = ((pr_x**2)+(pr_y**2)+(pr_z**2))**(0.5)
+                self.predkosc_ul = ((pr_x**2)+(pr_y**2)+(pr_z**2))**(0.5)/pr_swiatla
+                self.przyspieszenie_ms = ((a_x**2)+(a_y**2)+(a_z**2))**(0.5)
+                self.przyspieszenie_ul = ((a_x**2)+(a_y**2)+(a_z**2))**(0.5) / pr_swiatla
+                wektor=np.array([pr_x,pr_y,pr_z])
+                self.wektor_predkosci=wektor/wektor.max()
+        self.czynnik_lor = (1 - self.predkosc_ul ** 2) **(-0.5)
+        self.masa_spoczynkowa = mas_spo
+        # self.masa_relatywistyczna = self.masa_spoczynkowa * self.predkosc_ms
+        # self.energia_kinetyczna=(1/2)*self.masa*self.predkosc**2
+
+    def droga(self, t):
+        if self.przyspieszenie_ms == 0:
+            t_dylatacja = t/self.czynnik_lor
+            droga = self.predkosc_ms * t_dylatacja
+            droga_new=self.predkosc_ms*t
+        else:
+            droga = ((np.sqrt(1+(((self.przyspieszenie_ms * t + self.predkosc_ms * self.czynnik_lor)**2) \
+                                  / pr_swiatla**2)))- self.czynnik_lor)* (pr_swiatla ** 2)/self.przyspieszenie_ms
+            droga_new=self.predkosc_ms*t+(1/2)*self.przyspieszenie_ms*t**2
+        return droga,droga_new
+
+
+czasteczka = Czastka(0, 1, przysp = 0,typ=1,trojwymiar=1, pr_x=10000000, pr_y=5000000,pr_z=2000000)
+
+print(czasteczka.droga(1))
